27f7ac779d3f509506412112df7625fb7d4a7b86
yangyunjia <yyj980401@126.com>
2023-01-08 18:48:14 +0800

    add methods for initline

--- moc.py
+++ moc.py
@@ -9,10 +9,10 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
-from typing import Tuple, Callable
+from typing import Tuple, List, Callable
 
 GEOM = 0
-
+GAS_R = 1.4
 BIG_NUMBER = 100000.
 
 class ExtrapolateError(Exception):
@@ -36,6 +36,13 @@ class Node():
 
     def __repr__(self) -> str:
         return '(%.2f, %.2f) (%.2f, %.2f %.2f, %.2f)' % tuple(list(self.cors) + list(self.vals))
+
+    def set_by_total(self, tta: float, ma: float, pt: float, tt: float):
+        p, t, rho = calc_isentropicPTRHO(g=self.g, ma=ma, pTotal=pt, tTotal=tt)
+        self.rho = rho
+        self.p   = p
+        self.vel = ma * (self.g * GAS_R * t)**0.5
+        self.tta = tta
 
     @property
     def x(self) -> float:
@@ -159,7 +166,14 @@ class Node():
     @property
     def ma(self) -> float:
         return self.vel / self.a
-        
+
+def calc_isentropicPTRHO(g: float, ma: float, pTotal: float, tTotal: float):
+    ratio = 1 + (g - 1) / 2. * ma**2
+    p = pTotal / ratio**(g / (g - 1))
+    t = tTotal / ratio
+    rho = p / (GAS_R * t)
+    return p, t, rho
+
 def _calc_p3_xy(_p1: Node, _p2: Node, _p4: Node, tta0: float) -> float:
     return (_p4.y - _p2.y - tta0 * (_p4.x - _p2.x)) / (_p1.y - _p2.y - tta0 * (_p1.x - _p2.x))
 
@@ -266,6 +280,119 @@ def calc_sym_point(p1: Node, p3: Node) -> Node:
 
     return p4
 
+def calc_initial_throat_line(n: int, yw0: float, yc0: float = 0.0,
+             isTotal: bool = True, rUp: float = 0.0, pTotal: float = 0.0, tTotal: float = 0.0, mT: float = 0.0) -> List[Node]:
+    '''
+    This function is interpreted from code of CalcInitialThroatLine <- MOC_GidCalc_BDE <- the MOC programma of NASA
+    '''
+
+    _init_line: List[Node] = []
+
+    for i in range(n + 1):
+        # Based on the way it is done in RAO, the y[i] is based on a sinusoidal
+        # and x[i] is assumed to be on the RRC from the throat wall
+        
+        # input yy should be non-dimensional
+        _yy = math.sin(math.pi * (n - i) / n / 2.)**1.5
+
+        if i > 0:
+            dydx = _init_line[-1].lam_minus   # this calculates a new X first assumed it falls on the RRC
+
+        _mach = 2.0
+        point = Node()
+        if isTotal:
+            if i > 0:
+                while _mach > 1.5:
+                    _xx = _xx_old + (_yy - _yy_old) / dydx
+                    _theta, _mach = KLThroat(_xx, _yy, point.g, rUp)
+                    # increase dydx so that slope is half of what is was, only when mach number exceed 1.5
+                    # this is to prevent the initial line is too steep and lead to solution failure
+                    dydx *= 2.0
+            else:
+                _xx = 0.0
+                _theta, _mach = KLThroat(_xx, _yy, point.g, rUp)
+
+        else:
+            if mT < 1.0:
+                raise ValueError("Calculated Throat Mach number < 1.0\nCalcInitialThroatLine")
+            _theta = 0.0
+            _mach  = mT
+            if i > 0:
+                _xx = _xx_old.x + (_yy - _yy_old) / dydx
+            else:
+                _xx = 0.0
+        
+        point.x = (yw0 - yc0) * _xx
+        point.y = (yw0 - yc0) * _yy + yc0
+        _xx_old = _xx
+        _yy_old = _yy
+        # print(i, _theta, _mach)
+        point.set_by_total(tta=_theta, ma=_mach, pt=pTotal, tt=tTotal)
+
+        _init_line.append(point)
+
+    return _init_line
+                
+def KLThroat(x: float, y: float, G: float, RS: float) -> None:
+    '''
+    adapted from `int MOC_GridCalc::KLThroat(int i, int geom, double RS)` <- MOC_GidCalc_BDE <- the MOC programma of NASA
+    
+    This is taken from 'Transonic Flow in Small Throat Radius Curvature Nozzles',
+    by Kliegel and Levine. In this they take the HALL method and modify the Axi
+    calculation for a toroid coordinate system.  The 2D version is the one developed by HALL.
+
+    param:
+    ---
+    `y` is the radial distance of a point on the starting plane
+
+    '''
+
+    u = np.zeros(4)
+    v = np.zeros(4)
+
+    if (GEOM == 0):
+        #  Uses the modified Hall Method in toroid coordinates
+        z = x * (2 * RS / (G + 1))**0.5    # Eq. 12 in Hall paper
+        RSP = RS + 1
+        u[1] = y*y/2 - 0.25 + z
+        v[1] = y*y*y/4 - y/4 + y*z
+        u[2] = (2*G + 9)*y*y*y*y/24 - (4*G + 15)*y*y/24 + (10*G + 57)/288 + z*(y*y - 5/8) - (2*G - 3)*z*z/6
+        v[2] = (G + 3)*y*y*y*y*y/9 - (20*G + 63)*y*y*y/96 + (28*G + 93)*y/288 + z*((2*G + 9)*y*y*y/6 - (4*G + 15)*y/12) + y*z*z
+        u[3] = (556*G*G + 1737*G + 3069)*y*y*y*y*y*y/10368 - (388*G*G + 1161*G + 1881)*y*y*y*y/2304 + (304*G*G + 831*G + 1242)*y*y/1728 - (2708*G*G + 7839*G + 14211)/82944 + z*((52*G*G + 51*G + 327)*y*y*y*y/34 - (52*G*G + 75*G + 279)*y*y/192 + (92*G*G + 180*G + 639)/1152) + z*z*(-(7*G - 3)*y*y/8 + (13*G - 27)/48) + (4*G*G - 57*G + 27)*z*z*z/144
+        v[3] = (6836*G*G + 23031*G + 30627)*y*y*y*y*y*y*y/82944 - (3380*G*G + 11391*G + 15291)*y*y*y*y*y/13824 + (3424*G*G + 11271*G + 15228)*y*y*y/13824 - (7100*G*G + 22311*G + 30249)*y/82944 + z*((556*G*G + 1737*G + 3069)*y*y*y*y*y/1728 * (388*G*G + 1161*G + 1181)*y*y/576 + (304*G*G + 831*G + 1242)*y/864) + z*z*((52*G*G + 51*G + 327)*y*y*y/192 - (52*G*G + 75*G + 279)*y/192) - z*z*z*(7*G - 3)*y/12
+        
+        U = 1 + u[1]/RSP + (u[1] + u[2])/(RSP*RSP) + (u[1] + 2*u[2] + u[3])/(RSP*RSP*RSP)
+        V = ((G+1)/(2*RSP))*(v[1]/RSP + (1.5*v[1] + v[2])/(RSP*RSP) + (15./8.*v[1] + 2.5*v[2] + v[3])/(RSP*RSP*RSP))**0.5
+    
+    elif (GEOM == 1):
+        #  Calculate the z to be used in the velocity equations Eq 12
+        z = x * (RS / (G + 1))**0.5
+
+        u[1] = 0.5*y*y - 1/6 + z
+        v[1] = y*y*y/6 - y/6 + y*z
+        u[2] = (y+6)*y*y*y*y/18 - (2*G+9)*y*y/18 + (G+30)/270 + z*(y*y-0.5) - (2*G-3)*z*z/6
+        v[2] = (22*G+75)*y*y*y*y*y/360 - (5*G+21)*y*y*y/54 + (34*G+195)*y/1080 + z/9*((2*G+12)*y*y*y - (2*G+9)*y) + y*z*z
+        u[3] = (362*G*G+1449*G+3177)*y*y*y*y*y*y/12960 - (194*G*G + 837*G + 1665)*y*y*y*y/2592 + (854*G*G + 3687*G + 6759)*y*y/12960 - (782*G*G + 5523 + 2*G*2887)/272160 + z*((26*G*G + 27*G + 237)*y*y*y*y/288 - (26*G*G + 51*G + 189)*y*y/144 + (134*G*G + 429*G + 1743)/4320) + z*z*(-5*G*y*y/4 + (7*G - 18)/36) + z*z*z*(2*G*G - 33*G + 9)/72
+        v[3] = (6574*G*G + 26481*G + 40059)*y*y*y*y*y*y*y/181440 - (2254*G*G + 10113*G + 16479)*y*y*y*y*y/25920 + (5026*G*G + 25551*G + 46377)*y*y*y/77760 - (7570*G*G + 45927*G + 98757)*y/544320 +  z*((362*G*G + 1449*G + 3177)*y*y*y*y*y/2160 * (194*G*G + 837*G + 1665)*y*y*y/648 + (854*G*G + 3687*G + 6759)*y/6480) + z*z*((26*G*G + 27*G + 237)*y*y*y/144 - (26*G*G + 51*G + 189)/144) + z*z*z*(-5*G*y/6)
+        
+        U = 1 + u[1]/RS + u[2]/RS/RS + u[3]/RS/RS/RS
+        V = ((G+1)/RS)*(v[1]/RS + v[2]/RS/RS + v[3]/RS/RS/RS)**0.5
+
+    if (abs(V) < 1e-5): 
+        V = 0.0
+
+    _theta = math.atan2(V,U)
+
+    if (abs(_theta) < 1e-5):
+        _theta = 0.0
+
+    _ma = (U * U + V * V)**0.5
+
+    if _ma < 1.0:
+        raise ValueError("Intial Data Line is subsonic, try increasing Initial Line Angle")
+
+    return _theta, _ma
+
 
 class WallPoints():
 
@@ -320,35 +447,38 @@ class WallPoints():
         plt.plot(self.xx, self.yy, '-o', 'k')
         plt.show()
 
-        
-
 if __name__ == '__main__':
 
     eps = 1e-2
 
-    n = 8
-    x0 = np.zeros(n)
-    y0 = np.linspace(2., 0., n)
-    print(len(y0))
-    tta0 = np.zeros(n)
-    p0 = np.ones(n) * 101325
-    rho0 = np.ones(n) * 1.125
-    vel0 = np.ones(n) * (1.4 * p0 / rho0)**0.5 * 1.1
+    n = 12
 
+    ktta = 8.0
     upperwall = WallPoints()
-    upperwall.add_section(np.sin(np.linspace(0., math.pi / 12., 9)), lambda x: 3. - (1. - x**2)**0.5)
-    upperwall.add_section(np.linspace(0, 5, 12), lambda x: math.tan(math.pi / 12.) * x)
+    upperwall.add_section(np.sin(np.linspace(0., math.pi / 180. * ktta, 9)), lambda x: 8. - (6.**2 - x**2)**0.5)
+    upperwall.add_section(np.linspace(0, 5, 12), lambda x: math.tan(math.pi / 180. * ktta) * x)
     # upperwall.plot()
 
+    # x0 = np.zeros(n)
+    # y0 = np.linspace(2., 0., n)
+    # print(len(y0))
+    # tta0 = np.zeros(n)
+    # p0 = np.ones(n) * 101325
+    # rho0 = np.ones(n) * 1.125
+    # vel0 = np.ones(n) * (1.4 * p0 / rho0)**0.5 * 1.1
+    # init_line = [Node(x0[i], y0[i], rho0[i], p0[i], vel0[i], tta0[i]) for i in range(n)]
+    init_line = calc_initial_throat_line(n, 2.0, rUp=9., pTotal=2015., tTotal=2726.)
+    # plt.plot([pt.x for pt in init_line], [pt.y for pt in init_line], '-o')
+    # plt.plot([pt.x for pt in init_line], [pt.ma for pt in init_line], '-o')
+    # plt.show()
 
-    init_line = [Node(x0[i], y0[i], rho0[i], p0[i], vel0[i], tta0[i]) for i in range(n)]
     sym_node = copy.deepcopy(init_line[-1])
 
     grid_points = []
     grid_points.append(init_line)
 
     step = 0
-    max_step = 100
+    max_step = 3
     flag_upper_wall = True
 
     while(len(init_line) > 0 and step < max_step):
